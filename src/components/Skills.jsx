9830888989bf34f597feeb140ed415515ec44a4d
juliafan55 <juliafan55@gmail.com>
2022-06-24 15:06:30 -0400

    grammar fix

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -12,7 +12,7 @@ const Skills = () => {
             <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
                 <div>
                     <p className="text-4xl font-bold inline border-b-4 border-[#eebbc3]">Skills</p>
-                    <p className="py-4">// These are the technologies I've worked with</p>
+                    <p className="py-4">// These are the technologies I've worked with:</p>
                 </div>
 
                 {/* icons */}
