a6302de9344822f1e681b9818213bac5233f2ad7
juliafan55 <juliafan55@gmail.com>
2022-06-24 11:12:14 -0400

    project section template complete

---
+++ src/projects.js
@@ -0,0 +1,34 @@
+const projects = [
+    {
+        id: 1,
+        img: "",
+        name: "Digital Hub",
+        desc: "Blog app",
+        live: "/",
+        code: "https://github.com/juliafan55/digital-hub-frontend",
+    },
+    {
+        id: 2,
+        img:"",
+        name: "Atlantic",
+        desc: "Simple front-end ecommerce website using fakestore api.",
+        live: "https://atlantic-ecommerce.netlify.app/",
+        code: "https://github.com/juliafan55/react-ecommerce-site",
+    },
+    {
+        id: 3,
+        img:"",
+        name: "Popcorn Time",
+        desc: "Movie review app made for people to most movies and leave reviews.",
+        live: "https://sei-popcorn-time.herokuapp.com/movies",
+        code: "https://github.com/juliafan55/SEI-Project-2-IMDB",
+    },
+    {
+        id: 4,
+        img:"",
+        name: "Studio Ghibli Trivia",
+        desc: "Trivia game",
+        live: "/",
+        code: "https://github.com/juliafan55/javascript-trivia",
+    },
+]
